8d750dd3e313c36fb5fff76d6a9ec6d09da5b721
EC2 Default User <ec2-user@ip-172-16-37-228.us-east-2.compute.internal>
2018-12-22 22:53:43 +0000

    feat: add prediction fn

--- serve/predict.py
+++ serve/predict.py
@@ -85,10 +85,8 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-    result = model.forward(data)
-    print(result) 
-    print(len(result))
-          
-    result = np.round(result.numpy())
+    result = model(data)
+    
+    result = np.round(result.data.numpy())
 
     return result
